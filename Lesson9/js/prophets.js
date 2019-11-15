3fe5529407462dfc021b7baa74394b91bb84d737
jumpingjellyfish <ken15017@byui.edu>
2019-11-14 21:06:22 -0800

    update

--- Lesson9/js/prophets.js
+++ Lesson9/js/prophets.js
@@ -1,18 +1,32 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
-const prophets = jsonObject['prophets'];
 
 
 fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject);
-    for (let i = 0; i < prophets.length; i++) {
-        let card = document.createElement('section');}
-let h2 = document.createElement('h2');
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        const prophets = jsonObject['prophets'];
+        for (let i = 0; i < prophets.length; i++) {
+            let card = document.createElement('section');
+            let h2 = document.createElement('h2');
+            let h3 = document.createElement('h3');
+            let p = document.createElement('p');
+            let image = document.createElement('img');
+            let alt = document.createElement('alt');
 
-h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-card.appendChild(h2);
-document.querySelector('div.cards').appendChild(card);
-  });
+            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+            h3.textContent = 'Date of Birth:' + ' ' + prophets[i].birthdate;
+            p.textContent = 'Place of Birth:' + ' ' + prophets[i].birthplace;
+
+            card.appendChild(h2);
+            card.appendChild(h3);
+            card.appendChild(p);
+            card.appendChild(image);
+
+            image.setAttribute('src', prophets[i].imageurl);
+            image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + '-' + prophets[i].order);
+
+            document.querySelector('div.cards').appendChild(card);
+        }
+    });
